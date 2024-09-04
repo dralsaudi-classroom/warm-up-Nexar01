3825b62f0724bd8694830be146e19cc394e12d5b
Nawaf ALmutairi <146471037+Nexar01@users.noreply.github.com>
2024-09-04 17:46:40 +0300

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,9 +1,9 @@
-package com.example.project;
+opackage com.example.project;
 
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hello world!");
 	}
 
 }
